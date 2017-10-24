78299a7467d25c88715648fec8f3b190e8372bb7
Chao Fan <fanchao05@gmail.com>
2017-10-24 00:35:38 -0400

    added session reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,16 @@
+import  { RECEIVE_CURRENT_USER, login, signup, logout } from '../actions/session_actions.js';
+import merge from 'lodash/merge';
+
+const initialState = {};
+const SessionReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      let newState = merge({}, state, { current_user: action.user });
+      return newState;
+    default:
+      return state;
+  }
+}
+
+export default SessionReducer;
