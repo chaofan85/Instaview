65778041ceb46d5909dc35b01501e957e76834a3
Chao Fan <fanchao05@gmail.com>
2017-10-24 11:34:41 -0400

    added store

--- frontend/instaview.jsx
+++ frontend/instaview.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store =
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Instaview</h1>, root);
 });
