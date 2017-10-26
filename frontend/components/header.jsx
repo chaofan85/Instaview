45ac984e196d7126d5f3def4aa873eb965c932e4
Chao Fan <fanchao05@gmail.com>
2017-10-26 17:20:59 -0400

    updated header styling

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -8,8 +8,28 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <div className='herder-bg-top'></div>
-        <button onClick={this.props.logout}>logout</button>
+        <nav>
+          <div className="logo">
+            <div className='header-bg-logo'></div>
+            <div className='header-logo'>Instaview</div>
+          </div>
+
+          <div className="search-area">
+            <input className='search-bar' type='text'/>
+            <div>
+              <span className='search-icon'></span>
+              <span className='search-text'>Search</span>
+            </div>
+          </div>
+
+          <div className='header-icons'>
+             <span className='explore-icon'></span>
+             <span className='notification-icon'></span>
+             <span className='profile-icon'></span>
+          </div>
+
+          {/*<button onClick={this.props.logout}>logout</button>*/}
+        </nav>
       </header>
     );
   }
