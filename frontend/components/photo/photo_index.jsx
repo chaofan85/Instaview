0ec5beb362a01afc11db8fabf47d2bd1d62592d1
Chao Fan <fanchao05@gmail.com>
2017-10-26 08:37:00 -0400

    ready for pushing to heroku

--- frontend/components/photo/photo_index.jsx
+++ frontend/components/photo/photo_index.jsx
@@ -4,17 +4,12 @@ import PhotoUpload from './photo_upload';
 class PhotoIndex extends React.Component {
   constructor(props) {
     super(props);
-
-    // this.state = { renderUpload: false };
-    // this.handleClick = this.handleClick.bind(this);
   }
 
   render() {
     return (
       <div className='photo-index'>
-        <div className='photo-upload'>
-          <PhotoUpload />
-        </div>
+        <PhotoUpload />
         <div className='feed-index'>
         </div>
       </div>
