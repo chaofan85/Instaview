a9eaebf76398ae5a3c6db386c10fa368c41c57fa
Chao Fan <fanchao05@gmail.com>
2017-10-29 18:35:57 -0400

    did styling for profile page

--- frontend/components/user/profile_index.jsx
+++ frontend/components/user/profile_index.jsx
@@ -11,7 +11,23 @@ class ProfileIndex extends React.Component {
         <article className="profile-main">
           <section className="profile-header">
             <div className="user-profile-avatar"></div>
-            <div className="user-profile-info"></div>
+            <div className="user-profile-info">
+              <div className="user-profile-info-1">
+                <span className="user-profile-username">
+                  {this.props.currentUser.username}
+                </span>
+                <span className="edit-profile">Edit Profile</span>
+                <span className="setting-options"></span>
+              </div>
+              <ul className="user-profile-info-2">
+                <li>xxx posts</li>
+                <li>xxx followers</li>
+                <li>xxx following</li>
+              </ul>
+              <div className="user-profile-info-3">
+                <span></span>
+              </div>
+            </div>
           </section>
         </article>
       </div>
