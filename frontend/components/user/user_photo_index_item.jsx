b57022d5588d176c79351fb3ff358943712b6f2b
Chao Fan <fanchao05@gmail.com>
2017-10-29 22:34:36 -0400

    updated user profile page

--- frontend/components/user/user_photo_index_item.jsx
+++ frontend/components/user/user_photo_index_item.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const UserPhotoIndexItem = ({ photo }) => {
   console.log(photo);
   return (
-    <div className="photo-item"></div>
+    <div className="photo-item">
+      <img src={`${photo.image_url}`} />
+    </div>
   );
 };
 
