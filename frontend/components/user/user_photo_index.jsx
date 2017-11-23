c4bcc131a4ad0e9b847684c9a689647197d8dd42
Chao Fan <fanchao05@gmail.com>
2017-11-22 23:22:38 -0500

    fixed the photo order problem on profile page

--- frontend/components/user/user_photo_index.jsx
+++ frontend/components/user/user_photo_index.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import UserPhotoIndexItem from './user_photo_index_item';
 
 const UserPhotoIndex = ({ user, photos }) => {
-  const photosArray = Object.values(photos);
+  const photosArray = Object.values(photos).sort((a, b) => {
+    return Date.parse(b.created_at) - Date.parse(a.created_at);
+  });
+
   const userPhotos = photosArray.map(photo => {
     return (<UserPhotoIndexItem photo={photo} key={photo.photoId}/>);
   });
