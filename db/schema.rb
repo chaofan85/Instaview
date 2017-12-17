2a25eb4169f7b3f63a16a41ab425d8245344a722
Chao Fan <fanchao05@gmail.com>
2017-12-16 22:35:56 -0500

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -48,15 +48,15 @@ ActiveRecord::Schema.define(version: 20171030041301) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "location"
+    t.text "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_file_name"
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
-    t.integer "user_id", null: false
-    t.string "location"
-    t.text "caption"
     t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
