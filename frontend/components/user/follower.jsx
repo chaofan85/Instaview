29267ec5a9f65a985ab3fc8d344a131da7075488
Chao Fan <fanchao05@gmail.com>
2018-01-29 23:28:39 -0500

    added follower list panel

--- frontend/components/user/follower.jsx
+++ frontend/components/user/follower.jsx
@@ -4,12 +4,38 @@ class Follower extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = { renderUpload: false };
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ renderUpload: true });
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeModal() {
+    this.setState({ renderUpload: false });
+    document.body.style.overflow = 'visible';
   }
 
   render() {
     console.log(this.props.followers);
     return (
-      <div><span>{this.props.followers.length}</span> followers</div>
+      <div>
+        <div onClick={this.openModal}><span className="followers">
+          {this.props.followers.length}
+        </span>followers</div>
+
+        { this.state.renderUpload ? (
+          <div className='upload-panel'>
+            <div className='modal-form'>
+              <span className="modal-close"
+                onClick={this.closeModal}>&times;</span>
+            </div>
+            <div className="modal-bg"></div>
+          </div>) : null }
+      </div>
     );
   }
 
