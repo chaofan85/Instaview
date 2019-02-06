d611e62b9aed32e939280c9253a79a9a737b49f9
chaofan85 <fanchao05@gmail.com>
2019-02-05 19:55:48 -0500

    added modal for unfollow

--- frontend/components/user/follow_button.jsx
+++ frontend/components/user/follow_button.jsx
@@ -3,8 +3,21 @@ import React from 'react';
 class FollowButton extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { renderConfirm: false };
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
     this.follow = this.follow.bind(this);
     this.unfollow = this.unfollow.bind(this);
+  }
+
+  openModal() {
+    this.setState({ renderConfirm: true });
+    document.body.style.overflow = "hidden";
+  }
+
+  closeModal() {
+    this.setState({ renderConfirm: false });
+    document.body.style.overflow = "visible";
   }
 
   follow() {
@@ -18,19 +31,38 @@ class FollowButton extends React.Component {
   }
 
   render() {
-    if(this.props.user) {
+    if (this.props.user) {
       if (this.props.user.followed_by_current_user) {
         return (
-          <span className="unfollow-user"
-            onClick={this.unfollow}>Following</span>
+          <div className="unfollow-user-container">
+            <span className="unfollow-user" onClick={this.openModal}>
+              Following
+            </span>
+
+            {
+              this.state.renderConfirm ? (
+                <div className='modal-panel'>
+                  <div className='modal-form'>
+                    <div>Unfollow?</div>
+                    <span className="modal-close"
+                      onClick={this.closeModal}>&times;</span>
+                  </div>
+                  <div className="modal-bg"></div>
+                </div>
+              ) : null
+            } 
+          </div>
         );
       } else {
         return (
-          <span className="follow-user"
-            onClick={this.follow}>Follow</span>
+          <span className="follow-user" onClick={this.follow}>
+            Follow
+          </span>
         );
       }
-    } else {return null;}
+    } else {
+      return null;
+    }
   }
 }
 
